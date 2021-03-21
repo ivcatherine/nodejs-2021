95be4d13de4db51f1b2aae16a1996a5102cc8023
Ekaterina Ivanova <ekaterina_ivanova1@epam.com>
2021-03-21 23:11:24 +0300

    Homework 2. CRUD operations

--- hw1/task1.js
+++
@@ -1,3 +0,0 @@
-process.stdin.on('data', data => {
-    return data.slice(0,-1).reverse().toString()
-}).pipe(process.stdout)
--- hw1/task3.1.js
+++
@@ -1,5 +0,0 @@
-import { stdin } from 'process'
-
-stdin.on('data', data => {
-    return data.slice(0,-1).reverse().toString()
-}).pipe(process.stdout)
